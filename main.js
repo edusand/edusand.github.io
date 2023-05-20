37471e06e0b4efc94876c8cf195b921caf2dcb13
Jose Eduardo Sandoval Polanco <eduardsand.po@gmail.com>
2023-05-19 18:54:58 -0700

    navbar active state

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+navlinks = document.querySelectorAll("a");
+
+navlinks.forEach(function(link) {
+	// este corre solo una vez al iniciar
+	link.addEventListener("click", function() {
+		// cada que se de click desactivar demas botones activos	
+		navlinks.forEach(function(link) {
+			link.classList.remove("active");
+		});
+
+		// activar solo el seleccionado
+		link.classList.toggle("active");
+	});
+});
